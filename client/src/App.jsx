4cb91f95d1882f47dde95518160ce9315f2a0898
vikramdev772 <vikramdev772@gmail.com>
2026-01-02 10:59:02 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./App.css"
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import L from './ui/L';
+import Card from './components/Card';
+import Login from './pages/Login';
 function App() {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/truck" element={<L/>}/>
+      <Route path="/card" element={<Card/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
+
+    
+    </>
   )
 }
 
